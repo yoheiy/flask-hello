39ad7ea311cdc68803e9ea2b2324db3f7811de9c
Yamada Yohei <yamadayohei@gmail.com>
2017-04-10 21:45:23 +0900

    hello and google

--- flhello.py
+++ flhello.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return redirect('http://google.com/')
+    return 'Hello, world'
+
+@app.route('/<word>')
+def google(word):
+    return redirect('http://google.com/%s' % word)
 
 if __name__ == '__main__':
     app.run()
