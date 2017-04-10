2452984f612d74179aa54a6c396d155c271e50b8
Yamada Yohei <yamadayohei@gmail.com>
2017-04-10 21:37:58 +0900

    redirect to google

--- flhello.py
+++ flhello.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world.'
+    return redirect('http://google.com/')
 
 if __name__ == '__main__':
     app.run()
