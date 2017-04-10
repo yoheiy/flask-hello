038b3b42dec421a588d4fd64abb681099bc78961
Yamada Yohei <yamadayohei@gmail.com>
2017-04-10 22:16:51 +0900

    select from 3

--- flhello.py
+++ flhello.py
@@ -3,13 +3,26 @@ from flask import redirect
 
 app = Flask(__name__)
 
+db = {
+    0: 'http://flask.pocoo.org/',
+    1: 'http://jinja.pocoo.org/',
+    2: 'http://werkzeug.pocoo.org/',
+}
+
+hello_html = '''<ul>
+<li> 0 flask
+<li> 1 jinja
+<li> 2 werkzeug
+</ul>'''
+
 @app.route('/')
 def hello():
-    return 'Hello, world'
+    return hello_html
 
-@app.route('/<word>')
-def google(word):
-    return redirect('http://google.com/?q=%s' % word)
+@app.route('/<int:id>')
+def google(id):
+    uri = db.get(id) or 'http://www.pocoo.org/'
+    return redirect(uri)
 
 if __name__ == '__main__':
     app.run()
